99a1c7933f5acdfabd90fc29737a935b1ced7d59
James Harding <jamesvitaly1993@gmail.com>
2020-02-09 20:17:47 +0000

    Add coming soon

--- src/templates/review-template.js
+++ src/templates/review-template.js
@@ -109,7 +109,7 @@ const selectors = [{
 
 const ReviewTemplate = ({ className, data }) => {
     const { coffeeshop } = data.prismic;
-    return coffeeshop.has_rating && (
+    return coffeeshop.has_rating ? (
     <div className={className}>
         <SEO title={`A detailed review of ${coffeeshop.name[0].text}`} />
         <Header />
@@ -162,7 +162,7 @@ const ReviewTemplate = ({ className, data }) => {
         </section>
         <Signup showByDefault stopRevealBar={() => {}}/>
     </div>
-    );
+    ) : <h1>Coming Soon</h1>
 }
 
 export default styled(ReviewTemplate)`
