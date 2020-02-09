c220c3b454538874ea93fa471c42cab1c5e17876
James Harding <jamesvitaly1993@gmail.com>
2020-02-09 20:23:34 +0000

    Fixed build issue

--- src/templates/review-template.js
+++ src/templates/review-template.js
@@ -109,7 +109,7 @@ const selectors = [{
 
 const ReviewTemplate = ({ className, data }) => {
     const { coffeeshop } = data.prismic;
-    return coffeeshop.has_rating ? (
+    return coffeeshop && coffeeshop.has_rating ? (
     <div className={className}>
         <SEO title={`A detailed review of ${coffeeshop.name[0].text}`} />
         <Header />
