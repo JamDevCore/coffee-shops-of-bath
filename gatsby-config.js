876c2c0d9ab4395191e01e1e3260951ad2b9a08f
James Harding <jamesvitaly1993@gmail.com>
2020-02-10 09:29:19 +0000

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
           families: ['Playfair Display']
         }
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
