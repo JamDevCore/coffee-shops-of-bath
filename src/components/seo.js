1972fe333462526d75359af3bb504190206209f4
James Harding <jamesvitaly1993@gmail.com>
2020-02-12 13:55:25 +0000

    Added google analytics as raw script

--- src/components/seo.js
+++ src/components/seo.js
@@ -46,7 +46,17 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146538354-2"></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-146538354-2');`}
+        </script>
+
+      </Helmet>
   )
 }
 
