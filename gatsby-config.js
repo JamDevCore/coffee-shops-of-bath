97789c007fa3e4577d2d477920465a853a735b51
James Harding <jamesvitaly1993@gmail.com>
2020-02-20 18:07:38 +0000

    Added new sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,23 @@ module.exports = {
           }],
       }
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: [`/preview`, `/:uid`],
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + `${edge.node.path}/`,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
